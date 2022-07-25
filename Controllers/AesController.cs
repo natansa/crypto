812d740912338eeb4cb030b36da6c62837744c78
Natanael Sa Rodrigues <natanael.sa@gmail.com>
2022-07-25 11:17:23 -0300

    Implementação de criptografia AES

---
+++ Controllers/AesController.cs
@@ -0,0 +1,58 @@
+﻿using crypto.Crypto.Aes;
+using crypto.Crypto.Tripledes;
+using crypto.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace crypto.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AesController : ControllerBase
+    {
+        private readonly ILogger<AesController> _logger;
+        private readonly CryptoAes _cryptoAes;
+
+        public AesController(ILogger<AesController> logger)
+        {
+            _logger = logger;
+            _cryptoAes = new CryptoAes();
+        }
+
+        [HttpPost("criptografar")]
+        public IActionResult Criptografar([FromBody] DadosSensiveis dadosSensiveis)
+        {
+            var dadosSensiveisString = JsonSerializer.Serialize(dadosSensiveis);
+            _logger.LogInformation($"Dados sensíveis: {dadosSensiveisString}");
+
+            var resultado = new DadosSensiveis
+            {
+                Nome = _cryptoAes.Encrypt(dadosSensiveis.Nome),
+                Documento = _cryptoAes.Encrypt(dadosSensiveis.Documento),
+            };
+
+            var dadosCriptografados = JsonSerializer.Serialize(resultado);
+            _logger.LogInformation($"Dados sensíveis criptografados: {dadosCriptografados}");
+
+            return Ok(resultado);
+        }
+
+        [HttpPost("descriptografar")]
+        public IActionResult Descriptografar([FromBody] DadosSensiveis dadosSensiveis)
+        {
+            var dadosSensiveisString = JsonSerializer.Serialize(dadosSensiveis);
+            _logger.LogInformation($"Dados sensíveis criptografados: {dadosSensiveisString}");
+
+            var resultado = new DadosSensiveis
+            {
+                Nome = _cryptoAes.Decrypt(dadosSensiveis.Nome),
+                Documento = _cryptoAes.Decrypt(dadosSensiveis.Documento),
+            };
+
+            var dadosCriptografados = JsonSerializer.Serialize(resultado);
+            _logger.LogInformation($"Dados sensíveis descriptografados: {dadosCriptografados}");
+
+            return Ok(resultado);
+        }
+    }
+}
---
+++ Crypto/Aes/CryptoAes.cs
@@ -0,0 +1,76 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace crypto.Crypto.Aes
+{
+    public class CryptoAes
+    {
+        private static readonly string saltAES = "D330F679-2130-4EC8-A857-04170C0E9EB9";
+        private static string inputKey;
+
+        public CryptoAes()
+        {
+            inputKey = new string(saltAES.Reverse().ToArray());
+        }
+
+        public string Encrypt(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+
+            var aes = NewRijndaelManaged();
+            var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+            var memoryStreamEncrypt = new MemoryStream();
+            using (var csEncrypt = new CryptoStream(memoryStreamEncrypt, encryptor, CryptoStreamMode.Write))
+            using (var swEncrypt = new StreamWriter(csEncrypt))
+            {
+                swEncrypt.Write(value);
+            }
+
+            return Convert.ToBase64String(memoryStreamEncrypt.ToArray());
+        }
+
+        public string Decrypt(string value)
+        {
+            if (string.IsNullOrEmpty(value) || !IsBase64String(value))
+            {
+                return value;
+            }
+
+            var aes = NewRijndaelManaged();
+            var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+            var cipher = Convert.FromBase64String(value);
+            string result;
+
+            using (var memoryStreamDecrypt = new MemoryStream(cipher))
+            {
+                using var csDecrypt = new CryptoStream(memoryStreamDecrypt, decryptor, CryptoStreamMode.Read);
+                using var srDecrypt = new StreamReader(csDecrypt);
+                result = srDecrypt.ReadToEnd();
+            }
+
+            return result;
+        }
+
+        private RijndaelManaged NewRijndaelManaged()
+        {
+            var saltBytes = Encoding.ASCII.GetBytes(saltAES);
+            var key = new Rfc2898DeriveBytes(inputKey, saltBytes);
+
+            var aes = new RijndaelManaged();
+            aes.Key = key.GetBytes(aes.KeySize / 8);
+            aes.IV = key.GetBytes(aes.BlockSize / 8);
+
+            return aes;
+        }
+
+        private static bool IsBase64String(string base64String)
+        {
+            base64String = base64String.Trim();
+            return (base64String.Length % 4 == 0) && Regex.IsMatch(base64String, @"^[a-zA-Z0-9\+/]*={0,3}$", RegexOptions.None);
+        }
+    }
+}
