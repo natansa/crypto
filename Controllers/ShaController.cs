410a22a5c3aa49c82737ac75ea414459f299857b
Natanael Sa Rodrigues <natanael.sa@gmail.com>
2022-07-25 10:22:38 -0300

    Implementação de criptografia Sha 512

---
+++ Controllers/ShaController.cs
@@ -0,0 +1,37 @@
+﻿using crypto.Crypto.Sha;
+using crypto.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace crypto.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShaController : ControllerBase
+    {
+        private readonly ILogger<ShaController> _logger;
+
+        public ShaController(ILogger<ShaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("criptografar")]
+        public IActionResult Criptografar([FromBody] DadosSensiveis dadosSensiveis)
+        {
+            var dadosSensiveisString = JsonSerializer.Serialize(dadosSensiveis);
+            _logger.LogInformation($"Dados sensíveis: {dadosSensiveisString}");
+
+            var resultado = new DadosSensiveis
+            {
+                Nome = CryptoSha.Encrypt(dadosSensiveis.Nome),
+                Documento = CryptoSha.Encrypt(dadosSensiveis.Documento),
+            };
+
+            var dadosCriptografados = JsonSerializer.Serialize(resultado);
+            _logger.LogInformation($"Dados sensíveis criptografados: {dadosCriptografados}");
+
+            return Ok(resultado);
+        }
+    }
+}
---
+++ Crypto/Sha/CryptoSha.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace crypto.Crypto.Sha
+{
+    public static class CryptoSha
+    {
+        private static readonly string preSalt = "FCF2741E-0B74-424D-83F9-9EAA0C8B897F";
+        private static readonly string posSalt = "E603DCA8-1B6E-4AF9-AE35-6449FC1F84DF";
+
+        public static string Encrypt(string dataToEncrypt)
+        {
+            string encryptedData;
+
+            using (var sha512 = SHA512.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes($"{preSalt}{dataToEncrypt}{posSalt}");
+                var hash = sha512.ComputeHash(bytes);
+                encryptedData = GetStringFromHash(hash);
+            }
+
+            return encryptedData;
+        }
+
+        private static string GetStringFromHash(byte[] hash)
+        {
+            var result = new StringBuilder();
+
+            for (var i = 0; i < hash.Length; i++)
+            {
+                result.Append(hash[i].ToString("X2"));
+            }
+
+            return result.ToString();
+        }
+    }
+}
